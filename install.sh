92cacda16dd8daeffaa8c17899141de788e9756f
jsimonelli <jose.simonelli@gmail.com>
2015-07-31 15:31:10 -0400

    added buttons and service dialogs

--- install.sh
+++ install.sh
@@ -5,7 +5,14 @@ DOMAIN=miq-Marketplace
 install_cmd() {
     echo "Importing ${DOMAIN} from ${TOPDIR}/Automate/${DOMAIN}"
     cd /var/www/miq/vmdb
+    echo "Importing Automate"
     bin/rake "rhconsulting:miq_ae_datastore:import[${DOMAIN}, ${TOPDIR}/Automate]"
+
+    echo "Importing Service Dialogs"
+    bin/rake rhconsulting:dialogs:import[${TOPDIR}/ServiceDialogs]
+ 
+    echo "Importing Buttons"
+    bin/rake rhconsulting:buttons:import[${TOPDIR}/Buttons/buttons.yml]
 }
 
 if [ -d ${TOPDIR}/Automate/${DOMAIN} ] ; then
